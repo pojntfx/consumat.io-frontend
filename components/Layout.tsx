8204309494aff5a93f6e871190cc8c96efb532a7
Tobias Helmrich <tobias.g.helmrich@gmail.com>
2021-04-19 00:03:23 +0200

    feat: Implement navbar and basic navigation structure

---
+++ components/Layout.tsx
@@ -0,0 +1,16 @@
+import MetaData from './MetaData';
+import Navbar from './Navbar';
+
+const Layout = ({ children }) => {
+  return (
+    <>
+      <MetaData />
+      <Navbar />
+      <div>
+        {children}
+      </div>
+    </>
+  )
+}
+
+export default Layout
