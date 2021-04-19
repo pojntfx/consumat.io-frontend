f7ea22b1e8baa991683cf67bf7d6a725a6005a93
Tobias Helmrich <tobias.g.helmrich@gmail.com>
2021-04-19 21:38:33 +0200

    fix: Improve navbar hover effect

--- pages/search.tsx
+++ pages/search.tsx
@@ -1,12 +1,12 @@
-import MetaData from '../components/MetaData';
+import MetaData from "../components/MetaData";
 
 const Search = () => {
   return (
-    <div className='px-8'>
-      <MetaData title='consumat.io | Search' />
+    <div className="px-8">
+      <MetaData title="consumat.io | Search" />
       <h2>Hello, Search!</h2>
     </div>
   );
-}
+};
 
 export default Search;
