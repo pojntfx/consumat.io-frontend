1ca5f4eca2905938e74be8beabd9c5fc17312857
lucas-goldner <lucas.goldner@googlemail.com>
2021-04-19 19:18:51 +0200

    feat: Align poster + banner

--- pages/details.tsx
+++ pages/details.tsx
@@ -4,19 +4,16 @@ const Details = () => {
   return (
     <div>
       <MetaData title="consumat.io | Home" />
-
       <div className="w-screen h-screen flex flex-row flex-wrap bg-red-900">
-        <div className="w-screen w-2/3 ">
+        <div className="w-screen w-2/3 relative">
           <img
-            className="h-screen"
+            className="h-3/5 w-screen bg-repeat"
             src="https://img.17qq.com/images/hrscswawtcx.jpeg"
           />
-          <div className="md:w-1/3 w-full">
-            <img
-              className="rounded-lg shadow-lg antialiased"
-              src="https://i.pinimg.com/236x/5c/e7/97/5ce7972ccf38c44ff01b79d5227d462a.jpg"
-            />
-          </div>
+          <img
+            className="rounded-lg shadow-lg antialiased absolute top-96 left-10"
+            src="https://i.pinimg.com/236x/5c/e7/97/5ce7972ccf38c44ff01b79d5227d462a.jpg"
+          />
         </div>
       </div>
     </div>
