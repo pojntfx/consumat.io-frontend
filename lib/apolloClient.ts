da205f723b09d6827f855b435790bceef3c56640
Felicitas Pojtinger <felicitas@pojtinger.com>
2021-04-21 20:27:16 +0000

    feat: Add basic GraphQL integration with generated client

---
+++ lib/apolloClient.ts
@@ -0,0 +1,38 @@
+import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client";
+import { useMemo } from "react";
+
+let apolloClient: ApolloClient<any> | undefined;
+
+function createApolloClient(uri: string) {
+  return new ApolloClient({
+    ssrMode: typeof window === "undefined",
+    link: new HttpLink({
+      uri,
+    }),
+    cache: new InMemoryCache(),
+  });
+}
+
+export function initializeApollo(initialState = null, uri: string) {
+  const _apolloClient = apolloClient ?? createApolloClient(uri);
+
+  if (initialState) {
+    const existingCache = _apolloClient.extract();
+
+    _apolloClient.cache.restore({ ...existingCache, ...initialState });
+  }
+
+  if (typeof window === "undefined") return _apolloClient;
+
+  if (!apolloClient) apolloClient = _apolloClient;
+
+  return _apolloClient;
+}
+
+export function useApollo(initialState: any, uri: string) {
+  const store = useMemo(() => initializeApollo(initialState, uri), [
+    initialState,
+  ]);
+
+  return store;
+}
