a0b165a2b8aab0e7a21ae6a36cf919ba88a03605
Tobias Helmrich <tobias.g.helmrich@gmail.com>
2021-04-23 13:47:08 +0200

    fix: Fix problem where navigation item was not clickable even though hover state was displayed

--- components/navigation/NavListItem.tsx
+++ components/navigation/NavListItem.tsx
@@ -17,13 +17,12 @@ const NavListItem = ({
   setMobileNavbarVisibility = null,
 }: NavListItemProps) => {
   return (
-    <li
-      className={`${styles.navigationLink} ${
-        isMobile ? styles.navigationItemMobile : styles.navigationItem
-      } ${isActive && styles.active}`}
-    >
+    <li>
       <Link href={href}>
         <a
+          className={`${styles.link} ${
+            isMobile ? styles.navigationLinkMobile : styles.navigationLink
+          } ${isActive && styles.active}`}
           onClick={
             setMobileNavbarVisibility &&
             (() => setMobileNavbarVisibility(false))
