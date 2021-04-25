fc2c78e9c6d8a3d9e12000a0ffec7999e025f7f8
lucas-goldner <lucas.goldner@googlemail.com>
2021-04-26 00:10:51 +0200

    feat: Mostly move css to own module

--- pages/details/[id].tsx
+++ pages/details/[id].tsx
@@ -28,38 +28,24 @@ const Details = () => {
             />
           </div>
         </div>
-        <div className="z-40 flex flex-col absolute md:left-72 md:top-96">
-          <h1 className="text-center sm:text-left sm:ml-10 mb-10 text-black sm:px-0 sm:text-white text-8xl 2xl:bottom-0 mt-24 md:mt-10">
-            {data.movie.title}
-          </h1>
-          <p className="sm:ml-10 text-black text-xs md:text-2xl">
-            {data.movie.overview}
-          </p>
+        <div className={styles.detailsTextContainer}>
+          <h1 className={styles.detailsTitle}>{data.movie.title}</h1>
+          <p className={styles.detailsOverview}>{data.movie.overview}</p>
         </div>
         <div className=" h-96 mt-96 md:mt-56 md:ml-16">
           <div className="flex flex-col sm:flex-row">
-            <div className="flex flex-col items-center md:items-start">
-              <p className="bg-white  text-gray-800 font-semibold py-2 mb-2 px-4 border border-gray-400 rounded shadow">
-                {data.movie.releaseDate}
-              </p>
-              <p className="bg-white  text-gray-800 font-semibold py-2 mb-2 px-4 border border-gray-400 rounded shadow">
-                {data.movie.runtime}
-              </p>
-              <p className="bg-white  text-gray-800 font-semibold py-2 mb-2 px-4 border border-gray-400 rounded shadow">
-                {data.movie.voteAverage}
-              </p>
-              <p className="bg-white  text-gray-800 font-semibold py-2 mb-2 px-4 border border-gray-400 rounded shadow">
-                {data.movie.popularity}
-              </p>
+            <div className={styles.detailsRowContainer}>
+              <p className={styles.detailsStat}>{data.movie.releaseDate}</p>
+              <p className={styles.detailsStat}>{data.movie.runtime}</p>
+              <p className={styles.detailsStat}>{data.movie.voteAverage}</p>
+              <p className={styles.detailsStat}>{data.movie.popularity}</p>
               <a href={data.movie.tmdb} className="underline">
                 More Details
               </a>
             </div>
-            <div className="flex flex-col items-center md:items-start">
-              <div className="flex flex-col items-center md:items-start">
-                <p className="md:ml-40 bg-white  text-gray-800 font-semibold py-2 mb-2 px-4 border border-gray-400 rounded shadow">
-                  Genres
-                </p>
+            <div className={styles.detailsRowContainer}>
+              <div className={styles.detailsRowContainer}>
+                <p className={styles.detailsStatItemRowHeader}>Genres</p>
                 <div className="md:ml-40 flex flex-row">
                   {data.movie.genres.map(({ name }, key) => {
                     return (
@@ -67,8 +53,8 @@ const Details = () => {
                         key={key}
                         className={
                           key == 0
-                            ? "ml-0 bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded"
-                            : "ml-2 bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded"
+                            ? styles.detailsStatItem
+                            : styles.detailsStatItemML
                         }
                       >
                         {name}
@@ -77,10 +63,8 @@ const Details = () => {
                   })}
                 </div>
               </div>
-              <div className="flex flex-col items-center mt-2 md:items-start">
-                <p className="md:ml-40 bg-white  text-gray-800 font-semibold py-2 mb-2 px-4 border border-gray-400 rounded shadow">
-                  Cast
-                </p>
+              <div className={styles.detailsRowContainer}>
+                <p className={styles.detailsStatItemRowHeader}>Cast</p>
                 <div className="md:ml-40 flex flex-row">
                   {data.movie.cast.map(({ name }, key) => {
                     return (
@@ -90,8 +74,8 @@ const Details = () => {
                             key={key}
                             className={
                               key == 0
-                                ? "ml-0 bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded"
-                                : "ml-2 bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded"
+                                ? styles.detailsStatItem
+                                : styles.detailsStatItemML
                             }
                           >
                             {name}
@@ -104,10 +88,8 @@ const Details = () => {
                   })}
                 </div>
               </div>
-              <div className="flex flex-col items-center mt-2 md:items-start">
-                <p className="md:ml-40 bg-white  text-gray-800 font-semibold py-2 mb-2 px-4 border border-gray-400 rounded shadow">
-                  Director
-                </p>
+              <div className={styles.detailsRowContainer}>
+                <p className={styles.detailsStatItemRowHeader}>Director</p>
                 <div className="md:ml-40 flex flex-row">
                   {data.movie.directors.map(({ name }, key) => {
                     return (
@@ -115,8 +97,8 @@ const Details = () => {
                         key={key}
                         className={
                           key == 0
-                            ? "ml-0 bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded"
-                            : "ml-2 bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded"
+                            ? styles.detailsStatItem
+                            : styles.detailsStatItemML
                         }
                       >
                         {name}
