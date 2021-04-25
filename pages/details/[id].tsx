e528b106bbf6e3955543f413b119574500934da6
lucas-goldner <lucas.goldner@googlemail.com>
2021-04-25 15:02:00 +0200

    feat: Implement movie data from hook into details

--- pages/details/[id].tsx
+++ pages/details/[id].tsx
@@ -5,15 +5,8 @@ import { useMovie } from "../../hooks/DataHooks";
 const Details = () => {
   const router = useRouter();
   const { id } = router.query;
-  console.log(id);
-
-  const title = "Gintama";
-  const description =
-    "Gintama is the best Anime of all time 11/10, best comedy, best animation, best fights, great characters! Gintama is the best Anime of all time 11/10, best comedy, best animation, best fights, great characters! Gintama is the best Anime of all time 11/10, best comedy, best animation, best fights, great characters! Gintama is the best Anime of all time 11/10, best comedy, best animation, best fights, great characters! Gintama is the best Anime of all time 11/10, best comedy, best animation, best fights, great characters!";
-  const bannerImage =
-    "https://ninotaku.de/wp-content/uploads/2020/12/gintama-1.jpg";
-  const posterImage =
-    "https://i.pinimg.com/236x/5c/e7/97/5ce7972ccf38c44ff01b79d5227d462a.jpg";
+  const { data, loading, error } = useMovie(188927);
+  console.log(data);
 
   return (
     <>
@@ -23,19 +16,22 @@ const Details = () => {
           <div className="z-20 relative h-96 ">
             <img
               className="details-poster top-48 ml-16 xl:ml-10 absolute"
-              src={posterImage}
+              src={"https://image.tmdb.org/t/p/original" + data.movie.backdrop}
             />
           </div>
           <div className="absolute inset-0 h-auto z-10">
-            <img src={bannerImage} className="h-full w-full object-cover" />
+            <img
+              src={"https://image.tmdb.org/t/p/original" + data.movie.poster}
+              className="h-full w-full object-cover"
+            />
           </div>
         </div>
         <div className="z-40 flex flex-col absolute md:left-72 md:top-96">
           <h1 className="text-center sm:text-left sm:ml-10 mb-10 text-black sm:px-0 sm:text-white text-8xl 2xl:bottom-0 mt-24 md:mt-10">
-            {title}
+            {data.movie.title}
           </h1>
           <p className="sm:ml-10 text-black text-xs md:text-2xl">
-            {description}
+            {data.movie.overview}
           </p>
         </div>
         <div className=" h-96 mt-96 md:mt-56 md:ml-16">
