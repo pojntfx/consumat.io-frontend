4b8e3ddadda552c930b7cdd40fbe93f4e7217769
Jonas Held <johnluckpickerd@pm.me>
2021-04-26 17:43:01 +0200

    feat: Add tailwind configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@ module.exports = {
   purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      height: {
+        29: "7.25rem",
+        "line-base-3": "4.5rem",
+        "line-sm-3": "3.75rem",
+      },
+    },
   },
   variants: {
     extend: {
