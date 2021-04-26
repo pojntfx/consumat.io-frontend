1508f172bb230cec99a742411a176b76e9e03d98
Tobias Helmrich <tobias.g.helmrich@gmail.com>
2021-04-26 13:56:06 +0200

    feat: Implement query and hook for TV shows and rename TvFields fragment

--- hooks/DataHooks.tsx
+++ hooks/DataHooks.tsx
@@ -1,7 +1,17 @@
 import { useGetMovieQuery } from "../lib/api/consumat-io";
+import { useGetTvQuery } from "../lib/api/consumat-io";
 
 export function useMovie(code: number, country: string = "US") {
   return useGetMovieQuery({
+    variables: {
+      code: code,
+      country: country,
+    },
+  });
+}
+
+export function useTv(code: number, country: string = "US") {
+  return useGetTvQuery({
     variables: {
       code: code,
       country: country,
