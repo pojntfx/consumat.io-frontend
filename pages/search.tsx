324394d31111685aa3a8f087f452abdd9a3afc7b
Jonas Held <johnluckpickerd@pm.me>
2021-04-27 21:56:19 +0200

    feat: Implement search by title

--- pages/search.tsx
+++ pages/search.tsx
@@ -1,18 +1,42 @@
 import MetaData from "../components/MetaData";
-import SearchResultItem from "../components/search/SearchResultItem";
 import { useSearch } from "../hooks/DataHooks";
 import Spinner from "../components/helper/Spinner";
+import SearchResultList from "../components/search/SearchResultList";
+import { useState } from "react";
+import { useRouter } from "next/router";
 
 const Search = () => {
-  const { data, loading, error } = useSearch("Star Trek Beyond");
-
-  if (error) return <h2>Error</h2>;
-  if (loading) return <Spinner />;
+  const router = useRouter();
+  const { q } = router.query;
+  const [query, setQuery] = useState<string>("");
+  const { data, loading, error } = useSearch(q);
 
   return (
     <div className="px-4">
       <MetaData title="consumat.io | Search" />
-      <SearchResultItem searchResult={data.search[0]} />
+
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
+          router.push({ query: { q: query } });
+        }}
+      >
+        <input
+          type="search"
+          name="q"
+          placeholder="Search..."
+          aria-label="Search"
+          required
+          onChange={(event) => setQuery(event.target.value)}
+        />
+        <button type="submit">Search</button>
+      </form>
+
+      {loading ? (
+        <Spinner />
+      ) : data != null ? (
+        <SearchResultList searchResults={data.search} />
+      ) : null}
     </div>
   );
 };
