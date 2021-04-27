695ff2ff4e147a7f9f8b39e6918574132c4a4bac
Tobias Helmrich <tobias.g.helmrich@gmail.com>
2021-04-27 22:59:41 +0200

    chore: Add dummy code and content to test MediaList component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,88 @@
 import MetaData from "../components/MetaData";
+import MediaList, { Medium, MediumType } from "../components/MediaList";
+import { Episode, Movie, Season, Tv } from "../lib/api/consumat-io";
+import { useMovie, useTv } from "../hooks/DataHooks";
+import { useState, useEffect } from "react";
 
 const Home = () => {
+  const {
+    data: popularMovieData,
+    loading: popularMovieLoading,
+    error: popularMovieError,
+  } = useMovie(188927);
+  const [popularMovieItems, setPopularMovieItems] = useState(
+    new Array<Medium>()
+  );
+
+  const {
+    data: popularTvData,
+    loading: popularTvLoading,
+    error: popularTvError,
+  } = useTv(87917);
+  const [popularTvItems, setPopularTvItems] = useState(new Array<Medium>());
+
+  useEffect(() => {
+    if (!popularMovieLoading && popularMovieData) {
+      const movieItem = {
+        code: popularMovieData.movie.code,
+        title: popularMovieData.movie.title,
+        poster: popularMovieData.movie.poster,
+        type: MediumType.Movie,
+      };
+
+      setPopularMovieItems([
+        movieItem,
+        movieItem,
+        movieItem,
+        movieItem,
+        movieItem,
+        movieItem,
+        movieItem,
+        movieItem,
+        movieItem,
+        movieItem,
+      ]);
+    }
+  }, [popularMovieData, popularMovieLoading, popularMovieError]);
+
+  useEffect(() => {
+    if (!popularTvLoading && popularTvData) {
+      const tvItem = {
+        code: popularTvData.tv.code,
+        title: popularTvData.tv.name,
+        poster: popularTvData.tv.poster,
+        type: MediumType.TV,
+      };
+
+      setPopularTvItems([
+        tvItem,
+        tvItem,
+        tvItem,
+        tvItem,
+        tvItem,
+        tvItem,
+        tvItem,
+        tvItem,
+      ]);
+    }
+  }, [popularTvData, popularTvLoading, popularTvError]);
+
   return (
-    <div className="px-8">
+    <div className="px-4">
       <MetaData title="consumat.io | Home" />
-      <h2>Hello, Home!</h2>
+      <MediaList
+        title="MOST POPULAR MOVIES"
+        items={popularMovieItems}
+        loading={popularMovieLoading}
+        error={popularMovieError}
+      />
+
+      <MediaList
+        title="MOST POPULAR TV SHOWS"
+        items={popularTvItems}
+        loading={popularTvLoading}
+        error={popularTvError}
+      />
     </div>
   );
 };
