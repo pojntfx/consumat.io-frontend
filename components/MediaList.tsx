1f1efe5ab1316e1b08edc28ffb2983220c0c8d6e
Tobias Helmrich <tobias.g.helmrich@gmail.com>
2021-04-30 16:55:20 +0200

    feat: Temporarily change dummy data on home to use search for list; adjust UI

--- components/MediaList.tsx
+++ components/MediaList.tsx
@@ -1,8 +1,6 @@
 import { ApolloError } from "@apollo/client";
-import { useState, useEffect } from "react";
-import { useMovie } from "../hooks/DataHooks";
 import { imgSizes, useImage } from "../hooks/ImageHook";
-import { Episode, Movie, Season, Tv } from "../lib/api/consumat-io";
+import { GetSearchQuery } from "../lib/api/consumat-io";
 import Spinner from "./helper/Spinner";
 import MediumCard from "./MediumCard";
 
@@ -32,27 +30,30 @@ type MediaListProps = {
   title: string;
   loading: boolean;
   error: ApolloError;
-  items: Medium[];
+  items: GetSearchQuery;
 };
 
 const MediaList = ({ title, items, loading, error }: MediaListProps) => {
   if (error) return <h2>{error.message}</h2>;
 
   return (
-    <div className="mb-6 p-4 bg-white rounded shadow-md">
-      <h2 className="mb-3">{title}</h2>
+    <div className="my-8 px-4 pb-4 bg-white rounded shadow-md">
+      <h2 className="inline-block -mt-3 mb-3 h-10 leading-10 px-3 rounded bg-gray-800 text-gray-50 shadow">
+        {title}
+      </h2>
       {loading ? (
         <div>
           <Spinner />
         </div>
       ) : (
         <div className="grid gap-3 grid-cols-media-list justify-evenly">
-          {items.map((item) => {
+          {items.search.map((item) => {
             return (
               <MediumCard
                 key={item.code}
+                code={item.code}
                 title={item.title}
-                imgSrc={useImage(imgSizes.poster.w185, item.poster)}
+                imgSrc={useImage(imgSizes.poster.w185, item.posterPath)}
               />
             );
           })}
--- components/MediumCard.tsx
+++ components/MediumCard.tsx
@@ -1,22 +1,41 @@
+import Link from "next/link";
+import styles from "../styles/MediaList.module.css";
+
 type MediumCardProps = {
   key: number;
+  code: number;
   title: string;
   imgSrc: string;
 };
 
-const MediumCard = ({ key, title, imgSrc }: MediumCardProps) => {
+const MediumCard = ({ key, code, title, imgSrc }: MediumCardProps) => {
   return (
-    <div
-      key={key}
-      className="place-self-center w-32 flex flex-col items-center justify-center rounded bg-white shadow cursor-pointer hover:shadow-md duration-75"
-    >
-      <img
-        src={imgSrc}
-        alt={`${title} poster`}
-        className="object-fill w-full rounded-t"
-      />
-      <p className="self-start text-sm py-1 px-2 truncate w-full">{title}</p>
-    </div>
+    <Link href={`/details/${code}`}>
+      <div
+        key={key}
+        className={
+          styles.container +
+          " place-self-center w-32 flex flex-col items-center justify-center rounded cursor-pointer group"
+        }
+      >
+        <div className="w-32 h-48 overflow-hidden rounded">
+          <img
+            src={imgSrc}
+            alt={`${title} poster`}
+            className={
+              styles.img +
+              " bg-gray-500 h-full object-cover z-0 rounded shadow group-hover:shadow-md duration-75"
+            }
+          />
+        </div>
+        <p
+          className="bg-white z-10 text-sm py-2 px-1 text-center truncate w-full -mt-4 rounded shadow group-hover:shadow-md duration-75"
+          title={title}
+        >
+          {title}
+        </p>
+      </div>
+    </Link>
   );
 };
 
