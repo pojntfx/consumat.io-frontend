c0e5d819c37b405ebb9e3d6d0eff0350dab96fa9
Felicitas Pojtinger <felicitas@pojtinger.com>
2021-05-03 01:37:43 +0200

    feat: Add BACKEND_SECRET validation

---
+++ constants/authn.ts
@@ -0,0 +1,2 @@
+export const CONSUMATIO_NAMESPACE_HEADER = "X-Consumatio-Namespace";
+export const CONSUMATIO_SECRET_HEADER = "X-Consumatio-Secret";
--- pages/api/graphql/[[...slug]].ts
+++ pages/api/graphql/[[...slug]].ts
@@ -1,6 +1,10 @@
 import { createProxyMiddleware } from "http-proxy-middleware";
 import { NextApiRequest, NextApiResponse } from "next";
 import { getSession } from "next-auth/client";
+import {
+  CONSUMATIO_NAMESPACE_HEADER,
+  CONSUMATIO_SECRET_HEADER,
+} from "../../../constants/authn";
 
 const proxy = createProxyMiddleware({
   target: process.env.BACKEND_API_URL,
@@ -13,6 +17,9 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
 
   // If authorized, add headers & proxy the request
   if (session) {
+    req.headers[CONSUMATIO_NAMESPACE_HEADER] = session.user?.email;
+    req.headers[CONSUMATIO_SECRET_HEADER] = process.env.BACKEND_SECRET;
+
     return (proxy as Function)(req, res);
   }
 
