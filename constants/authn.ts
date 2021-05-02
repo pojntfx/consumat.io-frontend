3d0f082ef26a582fc75130241df0bb980d999165
Felicitas Pojtinger <felicitas@pojtinger.com>
2021-05-02 22:47:06 +0200

    feat: Pass authorization token to Apollo

---
+++ constants/authn.ts
@@ -0,0 +1,1 @@
+export const SESSION_TOKEN_COOKIE_NAME = "next-auth.session-token";
--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,5 +1,6 @@
 import NextAuth from "next-auth";
 import Providers from "next-auth/providers";
+import { SESSION_TOKEN_COOKIE_NAME } from "../../../constants/authn";
 
 export default NextAuth({
   providers: [
@@ -9,4 +10,15 @@ export default NextAuth({
       domain: process.env.AUTH0_DOMAIN,
     }),
   ],
+  cookies: {
+    sessionToken: {
+      name: SESSION_TOKEN_COOKIE_NAME,
+      options: {
+        httpOnly: false,
+        sameSite: "lax",
+        path: "/",
+        secure: true,
+      },
+    },
+  },
 });
