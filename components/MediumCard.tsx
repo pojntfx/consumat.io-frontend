436b6da8a7f921c8bdd35ae4f2561584066248ad
Jonas Held <johnluckpickerd@pm.me>
2021-05-04 14:31:05 +0200

    feat: Add TV detail page

--- components/MediumCard.tsx
+++ components/MediumCard.tsx
@@ -10,27 +10,29 @@ type MediumCardProps = {
 
 const MediumCard = ({ code, title, imageSrc }: MediumCardProps) => {
   return (
-    <Link href={`/details/${code}`}>
-      <div
-        className={
-          styles.container +
-          " place-self-center w-32 flex flex-col items-center justify-center rounded cursor-pointer group"
-        }
-      >
-        <div className="overflow-hidden rounded">
-          <MediaImage
-            imageSrc={imageSrc}
-            className={styles.img + " w-32 h-48"}
-          />
-        </div>
-
-        <p
-          className="bg-white z-10 text-sm py-2 px-1 text-center truncate w-full -mt-4 rounded shadow group-hover:shadow-md duration-75"
-          title={title}
+    <Link href={`/movie/${code}`}>
+      <a>
+        <div
+          className={
+            styles.container +
+            " place-self-center w-32 flex flex-col items-center justify-center rounded cursor-pointer group"
+          }
         >
-          {title}
-        </p>
-      </div>
+          <div className="overflow-hidden rounded">
+            <MediaImage
+              imageSrc={imageSrc}
+              className={styles.img + " w-32 h-48"}
+            />
+          </div>
+
+          <p
+            className="bg-white z-10 text-sm py-2 px-1 text-center truncate w-full -mt-4 rounded shadow group-hover:shadow-md duration-75"
+            title={title}
+          >
+            {title}
+          </p>
+        </div>
+      </a>
     </Link>
   );
 };
