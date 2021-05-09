5a2c45f9d9580db35df58d0dfa08edd96669cee4
lucas-goldner <lucas.goldner@googlemail.com>
2021-05-10 01:39:58 +0200

    feat: Add defaultLayout prop for all pages

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,9 +3,11 @@ import type { AppProps } from "next/app";
 import Layout from "../components/Layout";
 import { useApollo } from "../lib/apolloClient";
 import { Provider } from "next-auth/client";
+import { useState } from "react";
 import "../styles/globals.css";
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
+  const [defaultLayout, setDefaultLayout] = useState("default");
   const apolloClient = useApollo(
     pageProps.initialApolloState,
     process.env.PROXIED_API_URL
@@ -14,8 +16,8 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <Provider session={pageProps.session}>
       <ApolloProvider client={apolloClient}>
-        <Layout>
-          <Component {...pageProps} />
+        <Layout defaultLayout={defaultLayout}>
+          <Component {...pageProps} setDefaultLayout={setDefaultLayout} />
         </Layout>
       </ApolloProvider>
     </Provider>
