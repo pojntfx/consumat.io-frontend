1cd7d12de3bd8a94ff5d6966b0d555b3ff5bc455
Tobias Helmrich <tobias.g.helmrich@gmail.com>
2021-05-23 14:46:53 +0200

    feat: Start adding seasons to TV details page

---
+++ components/detail/SeasonOverview.tsx
@@ -0,0 +1,47 @@
+import { useState } from "react";
+import { imageSizes, useImage } from "../../hooks/ImageHook";
+import { Season } from "../../lib/api/consumat-io";
+import SeasonEpisodeList from "./SeasonEpisodeList";
+
+type SeasonOverviewProps = {
+  season: Season;
+};
+
+const SeasonOverview = ({ season }: SeasonOverviewProps) => {
+  const [isEpisodeListVisible, setIsEpisodeListVisible] = useState(false);
+
+  return (
+    <div className="mb-4">
+      <div
+        className="flex items-end h-32 rounded shadow hover:shadow-md duration-75"
+        style={{
+          backgroundImage: `linear-gradient(0deg, rgba(31, 41, 55, 0.8), rgba(31, 41, 55, 0.8)), url(${useImage(
+            imageSizes.poster.w780,
+            season.posterPath
+          )})`,
+          backgroundSize: "cover",
+        }}
+      >
+        <button
+          className="flex items-end w-full h-full p-2"
+          onClick={() => setIsEpisodeListVisible(!isEpisodeListVisible)}
+        >
+          <h4
+            className="text-white text-3xl text-left rounded"
+            style={{ textShadow: "2px 2px 2px rgba(31, 41, 55)" }}
+          >
+            {season.title}
+          </h4>
+        </button>
+      </div>
+      {isEpisodeListVisible && (
+        <SeasonEpisodeList
+          tvCode={season.tvCode}
+          seasonNumber={season.seasonNumber}
+        />
+      )}
+    </div>
+  );
+};
+
+export default SeasonOverview;
