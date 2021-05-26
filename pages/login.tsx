c8546b723f321c06e1953ba5b8db87ee7466df43
lucas-goldner <lucas.goldner@googlemail.com>
2021-05-26 19:16:06 +0200

    feat: Add basic login setup

--- pages/login.tsx
+++ pages/login.tsx
@@ -3,7 +3,16 @@ import { signIn, useSession } from "next-auth/client";
 const Login = () => {
   const [session] = useSession();
 
-  if (!session) return <div></div>;
+  if (!session)
+    return (
+      <div className="h-auto flex flex-col self-center items-center justify-center">
+        <h1>CONSUMAT.IO</h1>
+        <h2>Track, plan and enjoy content.</h2>
+        <button onClick={() => signIn(null, { callbackUrl: "/" })}>
+          SIGN IN
+        </button>
+      </div>
+    );
 
   return null;
 };
