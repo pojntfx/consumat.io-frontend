75ca7d14fec65e14297176ac09fb34fe5e1c57cd
lucas-goldner <lucas.goldner@googlemail.com>
2021-06-01 19:36:38 +0200

    feat: Add better login screen alignment

--- pages/login.tsx
+++ pages/login.tsx
@@ -6,7 +6,7 @@ const Login = () => {
   if (!session)
     return (
       <div className="h-auto flex flex-col self-center items-center justify-center">
-        <h2 className="text-4xl md:text-6xl mt-48">CONSUMAT.IO</h2>
+        <h2 className="text-4xl md:text-6xl mt-72">CONSUMAT.IO</h2>
         <p className="text-2xl md:text-3xl mt-2 font-light">
           Track, plan and enjoy content.
         </p>
