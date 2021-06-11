8334ea8fc80c32eaf3db488cb927ac0a3d6ac7f8
lucas-goldner <lucas.goldner@googlemail.com>
2021-06-11 23:06:06 +0200

    feat: Add language setting when logged in

--- hooks/AuthnHooks.tsx
+++ hooks/AuthnHooks.tsx
@@ -1,14 +1,23 @@
 import { useSession } from "next-auth/client";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
+import { Language } from "../types/language";
+import { useSetLanguage } from "./DataHooks";
 
 export function useAuthorization() {
   const [session, loading] = useSession();
   const router = useRouter();
+  const [updateLanguage, { data: de, loading: le, error: ee }] =
+    useSetLanguage();
 
   useEffect(() => {
     if (!(session || loading)) {
       router.push("/login");
+    }
+    if (session) {
+      updateLanguage({
+        variables: { language: "" + navigator.language },
+      });
     }
   }, [session, loading]);
 
