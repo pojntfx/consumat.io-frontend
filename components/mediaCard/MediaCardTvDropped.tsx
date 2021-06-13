38ca29242661c213ff80fd056e1181afb89a6a8a
Jonas Held <johnluckpickerd@pm.me>
2021-06-13 21:45:46 +0200

    feat: Add MediaCard placeholders

--- components/mediaCard/MediaCardTvDropped.tsx
+++ components/mediaCard/MediaCardTvDropped.tsx
@@ -1,12 +1,17 @@
 import React from "react";
 import { Tv } from "../../lib/api/consumat-io";
+import MediaCardWrapper from "./MediaCardWrapper";
 
 type MediaCardTvDroppedProps = {
   tv: Tv;
 };
 
 function MediaCardTvDropped({ tv }: MediaCardTvDroppedProps) {
-  return <div></div>;
+  return (
+    <MediaCardWrapper media={tv}>
+      <div></div>
+    </MediaCardWrapper>
+  );
 }
 
 export default MediaCardTvDropped;
