996b1761ae61a72e58c7d2cf645899e35fad0f9a
lucas-goldner <lucas.goldner@googlemail.com>
2021-06-15 00:33:35 +0200

    feat: Add multiple lists

--- pages/account.tsx
+++ pages/account.tsx
@@ -2,6 +2,7 @@ import { GetServerSideProps } from "next";
 import { getSession, signOut } from "next-auth/client";
 import { useAuthorization } from "../hooks/AuthnHooks";
 import {
+  useGetList,
   useGetUser,
   useGetWatchCount,
   useGetWatchTime,
@@ -18,6 +19,8 @@ import { Language } from "../types/language";
 import { Country } from "../types/country";
 import { useState, useEffect } from "react";
 import Spinner from "../components/helper/Spinner";
+import LibraryList from "../components/library/LibraryList";
+import { WatchStatus } from "../types/status";
 
 export const getServerSideProps: GetServerSideProps = async (context) => ({
   props: { session: await getSession(context) },
@@ -40,7 +43,16 @@ const Account = () => {
     useGetWatchCount(MediaType.Movie);
   const { data: watchCountTVData, loading: watchCountTVLoading } =
     useGetWatchCount(MediaType.Tv);
-  const { data, loading, error } = useGetUser();
+  const {
+    data: userData,
+    loading: userLoading,
+    error: userError,
+  } = useGetUser();
+  const {
+    data: listData,
+    loading: listLoading,
+    error: ListError,
+  } = useGetList(MediaType.Movie, WatchStatus.Finished);
 
   const allCountriesLabels = Country.map((item) => {
     return item.englishName;
@@ -54,99 +66,126 @@ const Account = () => {
   const allLanguagesIsos = Language.map((item) => {
     return item.iso;
   });
-  const [language, setLanguage] = useState(data?.user.country);
-  const [country, setCountry] = useState(data?.user.language);
-
-  useEffect(() => {
-    console.log("is Loading");
-  }, [loading]);
+  const [language, setLanguage] = useState(userData?.user.country);
+  const [country, setCountry] = useState(userData?.user.language);
 
   return (
     <>
-      {loading ||
+      {userLoading ||
       watchCountTVLoading ||
       watchCountMovieLoading ||
-      watchTimeTVLoading ? (
+      watchTimeTVLoading ||
+      listLoading ? (
         <Spinner />
       ) : (
-        <div className={styles.headerRow}>
-          {isSession(session) && (
-            <MediaImage className="w-60 h-60" imageSrc={session.user.image} />
-          )}
-          <div className="flex flex-col items-center md:items-start md:mr-auto md:ml-10">
+        <>
+          <div className={styles.headerRow}>
+            {isSession(session) && (
+              <MediaImage className="w-60 h-60" imageSrc={session.user.image} />
+            )}
+            <div className="flex flex-col items-center md:items-start md:mr-auto md:ml-10">
+              <div
+                className={
+                  "px-4 pb-4 bg-gradient-to-br from-white to-white dark:from-gray-700 dark:to-gray-800 rounded shadow-md mt-4 md:mt-0"
+                }
+              >
+                <h3 className="cardHeading">Statistics</h3>
+                <div className="flex flex-col justfiy-center">
+                  {isSession(session) && <h3>{session.user.name}</h3>}
+                  <StatistikItem
+                    title={"Watched Series"}
+                    times={watchCountTVData.watchCount}
+                  />
+                  <StatistikItem
+                    title={"Watched Movies"}
+                    times={watchCountMovieData.watchCount}
+                  />
+                  <StatistikItem
+                    title={"Total Watchtime"}
+                    times={
+                      (
+                        watchTimeTVData.watchTime / 60 +
+                        watchTimeMovieData.watchTime / 60
+                      ).toFixed(0) + "h"
+                    }
+                  />
+                </div>
+              </div>
+            </div>
+            <div className="flex flex-col" style={{ maxWidth: "10rem" }}>
+              <div
+                className={
+                  "px-4 pb-4 bg-gradient-to-br from-white to-white dark:from-gray-700 dark:to-gray-800 rounded shadow-md mt-4 md:mt-0"
+                }
+              >
+                <h3 className="cardHeading">Settings</h3>
+                <div className="flex flex-col">
+                  <label htmlFor="country" className="mr-1">
+                    Country:{" "}
+                  </label>
+                  <CustomSelectButton
+                    name="country"
+                    value={country}
+                    labels={allCountriesLabels}
+                    options={[userData?.user.country, ...allCountriesIsos]}
+                    onChange={({ target }) => {
+                      setCountry(target.value);
+                      updateCountry({ variables: { country: target.value } });
+                    }}
+                  />
+                  <label htmlFor="language" className="mr-1 ml-2">
+                    Language:{" "}
+                  </label>
+                  <CustomSelectButton
+                    className="mb-2"
+                    name="language"
+                    value={language}
+                    labels={allLanguagesLabels}
+                    options={[userData?.user.language, ...allLanguagesIsos]}
+                    onChange={({ target }) => {
+                      setLanguage(target.value);
+                      updateLanguage({ variables: { language: target.value } });
+                    }}
+                  />
+                  <button
+                    className={styles.logoutButton}
+                    onClick={() => signOut()}
+                  >
+                    Logout
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="flex flex-col md:flex-row max-width-md mt-4">
             <div
               className={
                 "px-4 pb-4 bg-gradient-to-br from-white to-white dark:from-gray-700 dark:to-gray-800 rounded shadow-md mt-4 md:mt-0"
               }
             >
-              <h3 className="cardHeading">Statistics</h3>
+              <h3 className="cardHeading">Finished Movies</h3>
               <div className="flex flex-col justfiy-center">
-                {isSession(session) && <h3>{session.user.name}</h3>}
-                <StatistikItem
-                  title={"Watched Series"}
-                  times={watchCountTVData.watchCount}
-                />
-                <StatistikItem
-                  title={"Watched Movies"}
-                  times={watchCountMovieData.watchCount}
-                />
-                <StatistikItem
-                  title={"Total Watchtime"}
-                  times={
-                    (
-                      watchTimeTVData.watchTime / 60 +
-                      watchTimeMovieData.watchTime / 60
-                    ).toFixed(0) + "h"
-                  }
+                <LibraryList
+                  mediaList={listData.list}
+                  watchStatus={WatchStatus.Finished}
                 />
               </div>
             </div>
-          </div>
-          <div className="flex flex-col" style={{ maxWidth: "10rem" }}>
             <div
               className={
-                "px-4 pb-4 bg-gradient-to-br from-white to-white dark:from-gray-700 dark:to-gray-800 rounded shadow-md mt-4 md:mt-0"
+                "px-4 pb-4 bg-gradient-to-br from-white to-white dark:from-gray-700 dark:to-gray-800 rounded shadow-md mt-4 md:mt-0 ml:0 md:ml-4"
               }
             >
-              <h3 className="cardHeading">Settings</h3>
-              <div className="flex flex-col">
-                <label htmlFor="country" className="mr-1">
-                  Country:{" "}
-                </label>
-                <CustomSelectButton
-                  name="country"
-                  value={country}
-                  labels={allCountriesLabels}
-                  options={[data?.user.country, ...allCountriesIsos]}
-                  onChange={({ target }) => {
-                    setCountry(target.value);
-                    updateCountry({ variables: { country: target.value } });
-                  }}
+              <h3 className="cardHeading">Finished Series</h3>
+              <div className="flex flex-col justfiy-center">
+                <LibraryList
+                  mediaList={listData.list}
+                  watchStatus={WatchStatus.Finished}
                 />
-                <label htmlFor="language" className="mr-1 ml-2">
-                  Language:{" "}
-                </label>
-                <CustomSelectButton
-                  className="mb-2"
-                  name="language"
-                  value={language}
-                  labels={allLanguagesLabels}
-                  options={[data?.user.language, ...allLanguagesIsos]}
-                  onChange={({ target }) => {
-                    setLanguage(target.value);
-                    updateLanguage({ variables: { language: target.value } });
-                  }}
-                />
-                <button
-                  className={styles.logoutButton}
-                  onClick={() => signOut()}
-                >
-                  Logout
-                </button>
               </div>
             </div>
           </div>
-        </div>
+        </>
       )}
     </>
   );
