b8365b0940393eaf539cf54fee34959706edfe61
lucas-goldner <lucas.goldner@googlemail.com>
2021-06-15 23:24:29 +0200

    feat: Add loadingDots and loading attributes to all statistics

--- components/dataDisplay/StatisticItem.tsx
+++ components/dataDisplay/StatisticItem.tsx
@@ -1,13 +1,22 @@
+import LoadingDots from "../feedback/LoadingDots";
+
 type StatistikItemProps = {
   title: string;
   times: number | string;
+  loading: boolean;
 };
 
-const StatistikItem = ({ title, times }: StatistikItemProps) => {
+const StatistikItem = ({ title, times, loading }: StatistikItemProps) => {
   return (
-    <div className="flex flex-row items-center">
-      <h3 className="mr-2">{title}</h3> <p className="ml-auto">{times}</p>
-    </div>
+    <>
+      {loading ? (
+        <LoadingDots />
+      ) : (
+        <div className="flex flex-row items-center">
+          <h3 className="mr-2">{title}</h3> <p className="ml-auto">{times}</p>
+        </div>
+      )}
+    </>
   );
 };
 
